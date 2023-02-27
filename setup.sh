ed3838d7973323cfb3c1c3c2ca78f5fc585da193
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-02-27 14:47:26 +0000

    Flesh out setup a bit

--- setup.sh
+++ setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+buildDir="build"
+sourceDir="source"
+
 echo "Starting build..."
 
-test -d build && rm -rf build
-mkdir build
+test -d $buildDir && { echo "Removing existing build directory"; rm -rf $buildDir; }
+mkdir $buildDir
 
-mkdir build/src
-mkdir build/launchpad
-mkdir build/docs-data
-mkdir build/cfg
+cp -r $sourceDir/* $buildDir/
+cp $sourceDir/.gitignore $buildDir/
+
+mkdir $buildDir/src
+mkdir $buildDir/launchpad
+mkdir $buildDir/launchpad/beta
+mkdir $buildDir/launchpad/release
+mkdir $buildDir/docs-data
+mkdir $buildDir/cfg
+mkdir $buildDir/docs
+
+cd $buildDir
+python3 scripts/docugen.py init
 
 echo "Build complete"
