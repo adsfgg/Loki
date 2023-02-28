d0b93db6b54a6d4a33dfe7ad2e1001fe065756c3
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-02-28 12:56:15 +0000

    Move source directory contents to root

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-buildDir="build"
-sourceDir="source"
-
+build_dir="build"
 echo "Starting build..."
 
-test -d $buildDir && { echo "Removing existing build directory"; rm -rf $buildDir; }
-mkdir $buildDir
+test -d $build_dir && { echo "Removing existing build directory"; rm -rf $build_dir; }
+mkdir $build_dir
 
-cp -r $sourceDir/* $buildDir/
-cp $sourceDir/.gitignore $buildDir/
-cp -r configs $buildDir/configs
+cp -r configs $build_dir/
+cp -r launchpad $build_dir/
+cp -r scripts $build_dir/
 
-mkdir $buildDir/src
-mkdir $buildDir/launchpad
-mkdir $buildDir/launchpad/beta
-mkdir $buildDir/launchpad/release
-mkdir $buildDir/docs-data
-mkdir $buildDir/docs
+cp check_for_outdated_vars.sh $build_dir/
+cp createBuild.sh $build_dir/
+cp gendocs.sh $build_dir/
+cp gitignore $build_dir/.gitignore
 
-cd $buildDir
+mkdir $build_dir/src
+mkdir $build_dir/docs-data
+mkdir $build_dir/docs
+
+cd $build_dir
 python3 scripts/docugen.py init
 
 echo "Build complete"
