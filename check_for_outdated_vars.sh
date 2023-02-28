fca3f2a153086478b0bf2e0d39130dd37fcfe060
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-02-28 13:41:10 +0000

    Convert var_checker to use config

--- check_for_outdated_vars.sh
+++ check_for_outdated_vars.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-. scripts/shared_funcs.sh
-
-install_path="$(get_ns2_install_path)"
-mod_src_dir="$(load_config_entry mod_src_dir)"
-balance_lua_file="$(load_config_entry balance_lua_file)"
-
-python3 scripts/var_checker.py \
-    "$mod_src_dir" \
-    "$balance_lua_file" \
-    "$install_path/ns2/lua/Balance.lua" \
-    "$install_path/ns2/lua/BalanceHealth.lua" \
-    "$install_path/ns2/lua/BalanceMisc.lua"
+python3 scripts/var_checker.py
 exit $?
--- scripts/config_loader/__init__.py
+++ scripts/config_loader/__init__.py
@@ -1,1 +1,1 @@
-from . import config_loader
+from .config_loader import load_docugen_config, load_general_config, get_ns2_install_path
